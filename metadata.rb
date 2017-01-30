bf277934bc4e61e43d8c4616f3c61108336b46db
root <root@chefwork.gateway.sonic.net>
2017-01-30 02:37:40 -0500

    init

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'starcon-cookbooks'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures starcon-cookbooks'
+long_description 'Installs/Configures starcon-cookbooks'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: starcon-cookbooks
+# Recipe:: default
+#
+# Copyright (C) 2017 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
